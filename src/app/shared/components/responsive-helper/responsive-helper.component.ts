42dce21ff7dd8183076c16a8de8053af9a82c939
Touafic BOUAB <eclairetoilegentlemans@gmail.com>
2024-07-02 10:07:13 -0400

    Responsive-helper component initialized

---
+++ src/app/shared/components/responsive-helper/responsive-helper.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-responsive-helper',
+  templateUrl: './responsive-helper.component.html',
+  styleUrls: ['./responsive-helper.component.scss']
+})
+export class ResponsiveHelperComponent {
+
+}
